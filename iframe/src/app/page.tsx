9c208ff7d94215c30f33d4275337a841520e68b2
fibbbonachos <arish2k3@gmail.com>
2023-09-15 14:57:21 +0530

    update: add iframe and pause button wip

--- iframe/src/app/page.tsx
+++ iframe/src/app/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+
+import { useRef } from "react";
+
 export default function Home() {
+    const ref = useRef(null);
+
+    const pauseVideo = () => {
+        ref.current.contentWindow.postMessage(
+            '{"event":"command","func":"' + "pauseVideo" + '","args":""}'
+        );
+    };
+
     return (
         <div>
-            <iframe src=""></iframe>
+            <iframe
+                id="ytplayer"
+                // type="text/html"
+                width="1000"
+                height="1000"
+                src="https://www.youtube.com/embed/SR6iYWJxHqs"
+                ref={ref}
+            ></iframe>
+            <button
+                onClick={() => {
+                    pauseVideo();
+                }}
+            >
+                pause
+            </button>
         </div>
     );
 }
