ad7e3897ce27679138ddb58031b4b969889d3afa
fibbbonachos <arish2k3@gmail.com>
2023-09-16 14:27:35 +0530

    update

--- iframe/src/app/page.tsx
+++ iframe/src/app/page.tsx
@@ -3,74 +3,12 @@
 import { useEffect, useRef } from "react";
 
 export default function Home() {
-    const videoRef = useRef(null);
-
-    const pauseVideo = () => {
-        videoRef.current.contentWindow.postMessage(
-            '{"event":"command","func":"pauseVideo","args":""}',
-            "*"
-        );
-    };
-    const playVideo = () => {
-        videoRef.current.contentWindow.postMessage(
-            '{"event":"command","func":"playVideo","args":""}',
-            "*"
-        );
-    };
-
-    const seekTo = (timeStamp: number) => {
-        videoRef.current.contentWindow.postMessage(
-            `{"event":"command","func":"seekTo","args":[${timeStamp}, true]}`,
-            "*"
-        );
-    };
-
-    const getDuration = () => {
-        videoRef.current.contentWindow.postMessage(
-            `{"event":"command","func":"getDuration","args":""}`,
-            "*"
-        );
-    };
-
     return (
         <div>
-            <iframe
-                id="ytplayer"
-                width="1000"
-                height="1000"
-                src="https://www.youtube.com/embed/SR6iYWJxHqs?enablejsapi=1"
-                // allow autoplay for fixing autoplay bug for playVideo function
-                allow="autoplay"
-                ref={videoRef}
-            ></iframe>
-            <button
-                onClick={() => {
-                    pauseVideo();
-                }}
-            >
-                pause
-            </button>
-            <button
-                onClick={() => {
-                    playVideo();
-                }}
-            >
-                play
-            </button>
-            <button
-                onClick={() => {
-                    seekTo(20);
-                }}
-            >
-                seek to
-            </button>
-            <button
-                onClick={() => {
-                    console.log(getDuration());
-                }}
-            >
-                get duration
-            </button>
+            <button onClick={() => {}}>Play</button>
+            <button onClick={() => {}}>Pause</button>
+            <button onClick={() => {}}>Stop</button>
+            <button onClick={() => {}}>Get Duration</button>
         </div>
     );
 }
