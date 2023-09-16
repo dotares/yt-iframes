ac36933e3db2bef21bc15388334143ea75e25b11
fibbbonachos <arish2k3@gmail.com>
2023-09-16 14:55:39 +0530

    update: success

--- iframe/src/app/page.tsx
+++ iframe/src/app/page.tsx
@@ -1,14 +1,44 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import ReactPlayer from "react-player";
 
 export default function Home() {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const ref = useRef(null);
+
     return (
-        <div>
-            <button onClick={() => {}}>Play</button>
-            <button onClick={() => {}}>Pause</button>
+        <>
+            <ReactPlayer
+                url="https://www.youtube.com/watch?v=SR6iYWJxHqs"
+                width="1000px"
+                height="1000px"
+                className="player"
+                playing={isPlaying}
+                ref={ref}
+            />
+            <button
+                onClick={() => {
+                    setIsPlaying(true);
+                }}
+            >
+                Play
+            </button>
+            <button
+                onClick={() => {
+                    setIsPlaying(false);
+                }}
+            >
+                Pause
+            </button>
             <button onClick={() => {}}>Stop</button>
-            <button onClick={() => {}}>Get Duration</button>
-        </div>
+            <button
+                onClick={() => {
+                    console.log(ref.current.getDuration());
+                }}
+            >
+                Get Duration
+            </button>
+        </>
     );
 }
